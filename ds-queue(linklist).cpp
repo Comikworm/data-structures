24197e882cbe227f22786b6f0855f97c14c178e0
Comikworm <ketakikenjle@gmail.com>
2026-04-07 09:25:15 +0530

    Format main function for better readability

---
+++ ds-queue(linklist).cpp
@@ -0,0 +1,70 @@
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = rear = NULL;
+    }
+
+    // ENQUEUE
+    void enqueue(int val) {
+        Node* newNode = new Node(val);
+
+        if (rear == NULL) {
+            front = rear = newNode;
+            return;
+        }
+
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    // DEQUEUE
+    void dequeue() {
+        if (front == NULL) {
+            cout << "Queue Underflow\n";
+            return;
+        }
+
+        cout << "Deleted: " << front->data << endl;
+        front = front->next;
+
+        if (front == NULL) {
+            rear = NULL;
+        }
+    }
+
+    // DISPLAY
+    void display() {
+        Node* temp = front;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL\n";
+    }
+};
+int main() {
+  // Queue
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.display();
+    q.dequeue();
+    q.display();
+
+    return 0;
+}
