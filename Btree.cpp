55c6c95871b951893e19b28eeb94ee3133d6ec7d
Comikworm <ketakikenjle@gmail.com>
2026-05-06 12:28:22 +0530

    Add delete and search options to B-tree menu

--- Btree.cpp
+++ Btree.cpp
@@ -7,7 +7,7 @@ int main(){
     int ch,x;
 
     do{
-        cout<<"\n1.Insert\n2.Display\n3.Exit\n";
+        cout<<"\n1.Insert\n2.Delete\n3.Search\n4.Display\n5.Exit\n";
         cin>>ch;
 
         switch(ch){
@@ -18,10 +18,25 @@ int main(){
                 break;
 
             case 2:
+                cout<<"Enter element: ";
+                cin>>x;
+                btree.erase(x);
+                break;
+
+            case 3:
+                cout<<"Enter element: ";
+                cin>>x;
+                if(btree.find(x)!=btree.end())
+                    cout<<"Found";
+                else
+                    cout<<"Not Found";
+                break;
+
+            case 4:
                 cout<<"B-Tree: ";
                 for(int i:btree)
                     cout<<i<<" ";
                 break;
         }
-    }while(ch!=3);
+    }while(ch!=5);
 }
